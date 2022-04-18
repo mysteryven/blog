a29002244dac945df467184cce08668efb8515e0
wenzhe <mysteryven@gmail.com>
2022-04-18 21:16:15 +0800

    chore: update

--- src/type-challenges/04.ts
+++ src/type-challenges/04.ts
@@ -18,3 +18,14 @@ type First<T extends any[]> = T extends [] ? never : T[0]
 
 // 18
 type Length<T extends {length: number}> = T['length']
+
+// 43
+// 其实我有点不理解，目前是把它当成了分配律
+// a' | 'b' | 'c', 'a'
+// a 在后面吗？在，never, b 在吗，不在，c 在吗，不在，然后 合并起结果
+type MyExclude<T, U> = T extends U ? never : T
+
+
+// 189 
+// 有个递归，，，小烦
+type MyAwaited<T> = T extends Promise<infer A> ? MyAwaited<A> : T
